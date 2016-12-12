4f76dca18f40eeca0f9f5f96452ea87bebbf771e
MonaLengelle <mona.lengelle@gmail.com>
2016-12-12 15:10:36 +0100

    nav class

--- src/batailleNavale/Navire.java
+++ src/batailleNavale/Navire.java
@@ -1,10 +1,42 @@
 package batailleNavale;
 
 public class Navire {
-
-	/**
-	 * @param args
-	 */
+	private Coordonnee debut;
+	private Coordonnee fin;
+	private Coordonnee[] partiesTouchees;
+	private int nbTouchees;
+	
+	public Navire(Coordonnee debut, int longueur, boolean estVertical) {
+		
+	}
+	public Coordonnee getDebut() {
+		
+	}
+	public Coordonnee getFin() {
+		
+	}
+	public boolean contient(Coordonnee c) {
+		
+	}
+	public boolean touche(Navire n) {
+		
+	}
+	public boolean chevauche(Navire n) {
+		
+	}
+	public boolean recoitTir(Coordonnee c) {
+		
+	}
+	public boolean estTouche(Coordonnee c) {
+		
+	}
+	public boolean estTouche() {
+		
+	}
+	public boolean estCoule() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
