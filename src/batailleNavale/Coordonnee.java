379a6e85b154d0402e44fe05008144b81d139c9f
cclmence <clemencechambonnet@gmail.com>
2016-12-12 13:25:13 +0100

    add squelette2

--- src/batailleNavale/Coordonnee.java
+++ src/batailleNavale/Coordonnee.java
@@ -6,16 +6,36 @@ public class Coordonnee {
 	private int colonne;
 
 	public Coordonnee(int ligne, int colonne) {
-		
+
 	}
-	
+
 	public Coordonnee(String s) {
 		// TODO Auto-generated constructor stub
 	}
 
+	public String toString() {
+	
+	}
+
+	public int getLigne() {
+	
+	}
+
+	public int getColonne() {
+
+	}
+
+	public boolean equals(Object obj) {
+
+	}
+	
+	public boolean voisine(Coordonnee c) {
+		
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Fuck GitT
+		
 	}
 
 }
