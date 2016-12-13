a82acfd9a5fde8e7b27b3ea0e167bef2cfc7c8db
cclmence <clemencechambonnet@gmail.com>
2016-12-13 09:10:22 +0100

    add method

--- src/batailleNavale/Navire.java
+++ src/batailleNavale/Navire.java
@@ -5,38 +5,68 @@ public class Navire {
 	private Coordonnee fin;
 	private Coordonnee[] partiesTouchees;
 	private int nbTouchees;
-	
+
 	public Navire(Coordonnee debut, int longueur, boolean estVertical) {
-		
+
 	}
+
 	public Coordonnee getDebut() {
-		
+		return debut;
 	}
+
 	public Coordonnee getFin() {
-		
+		return fin;
 	}
+
 	public boolean contient(Coordonnee c) {
-		
+
 	}
+
 	public boolean touche(Navire n) {
-		
+		int lgh = n.fin.getLigne() - n.debut.getLigne();
+		int lgv = n.fin.getColonne() - n.debut.getColonne();
+
+		if (lgh == 0) {
+			if (n.debut.voisine(this.debut) || n.fin.voisine(this.debut) || n.fin.voisine(this.fin)) {
+				return true;
+			}
+			Coordonnee i = n.debut;
+			while(i.compareTo(n.debut) < 0){
+				
+			} 
+		}
 	}
+
 	public boolean chevauche(Navire n) {
-		
+		int lgh = n.fin.getLigne() - n.debut.getLigne();
+		int lgv = n.fin.getColonne() - n.debut.getColonne();
+
+		if (lgh == 0) {
+			if (!n.fin.equals(this) || n.debut.contient()) {
+				return false;
+			}
+
+		} else {
+
+		}
 	}
+
 	public boolean recoitTir(Coordonnee c) {
-		
+
 	}
+
 	public boolean estTouche(Coordonnee c) {
-		
+
 	}
+
 	public boolean estTouche() {
-		
+
 	}
+
 	public boolean estCoule() {
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
