0115f1ffc753288ab28d2b10f063bc109ba1b902
C-c-l <cecilia.mcszk@gmail.com>
2016-12-16 10:17:35 +0100

    add taille

--- src/batailleNavale/GrilleNavaleGraphique.java
+++ src/batailleNavale/GrilleNavaleGraphique.java
@@ -3,10 +3,11 @@ public class GrilleNavaleGraphique extends GrilleNavale {
 	private GrilleGraphique grille;
 	
 	public GrilleNavaleGraphique(int taille) {
-		
+		// On initialise le 2e argument a 0 car on ne peut pas rien mettre
+		super(taille, 0);
 	}
 	public GrilleGraphique getGrilleGraphique() {
-		
+		return this.grille;
 	}
 	public boolean ajouteNavire(Navire n) {
 		
