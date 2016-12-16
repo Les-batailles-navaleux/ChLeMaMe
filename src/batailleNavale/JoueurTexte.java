d8ac15cec7ba592ac6ad528d0ef601fe8580eb45
cclmence <clemencechambonnet@gmail.com>
2016-12-16 09:25:57 +0100

    add joueur

--- src/batailleNavale/JoueurTexte.java
+++ src/batailleNavale/JoueurTexte.java
@@ -19,6 +19,7 @@ public class JoueurTexte extends Joueur {
 
 	protected void gagne() {
 		System.out.println("Vous avez gagne !");
+	}	
 	
 	protected void retourAttaque(Coordonnee c, int etat) {
 //			System.out.println("Le tir de votre adversaire en " + c + " est " + etat + ".");
@@ -61,19 +62,21 @@ public class JoueurTexte extends Joueur {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//GrilleNavale g = new GrilleNavale(10, 6);
-		//Coordonnee a1 = new Coordonnee("D4");
-		//Navire n1 = new Navire(a1, 4, true);
 		//g.ajouteNavire(n1);
 		//JoueurTexte j = new JoueurTexte(g, "DBardou");
 		//j.debutAttaque();
 		
 		int[] tN = {1};
 		GrilleNavale g1 = new GrilleNavale(10, tN);
-		g1.placementAuto(tN);
+		Coordonnee a1 = new Coordonnee("D4");
+		Navire n1 = new Navire(a1, 4, true);
+		g1.ajouteNavire(n1);
 		System.out.println("Grille joueur 1 : \n" + g1 + "\n");
 		
 		GrilleNavale g2 = new GrilleNavale(10, tN);
-		g2.placementAuto(tN);
+		Coordonnee a2 = new Coordonnee("A3");
+		Navire n2 = new Navire(a2, 3, true);
+		g2.ajouteNavire(n2);
 		System.out.println("Grille joueur 2 : \n" + g2 + "\n");
 		Joueur j1 = new JoueurTexte(g1, "Kris");
 		Joueur j2 = new JoueurTexte(g2, "Mik");
