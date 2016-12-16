564b4eea07da661528b16a969065bb50d5d499de
C-c-l <cecilia.mcszk@gmail.com>
2016-12-16 10:25:26 +0100

    fix teacher typo

--- src/batailleNavale/GrilleNavaleGraphique.java
+++ src/batailleNavale/GrilleNavaleGraphique.java
@@ -6,10 +6,12 @@ public class GrilleNavaleGraphique extends GrilleNavale {
 		// On initialise le 2e argument a 0 car on ne peut pas rien mettre
 		super(taille, 0);
 	}
-	public GrilleGraphique getGrilleGraphique() {
+	
+	public GrilleNavaleGraphique getGrilleGraphique() {
+		return this.grille;
 	}
+	
 	public boolean ajouteNavire(Navire n) {
-		
 	}
 	public boolean recoitTir(Coordonnee c) {
 		
